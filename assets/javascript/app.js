ce6536b59e14eb27c151f2aa1181e4a5e3648f3d
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2019-11-07 21:03:15 -0600

    make page responsive to phone and make colum for page and change some css parts

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyAM5HFiBF7iP96i-T_Z5q77Ijwj5FASDR0",
+    authDomain: "nutritfit-a2eef.firebaseapp.com",
+    databaseURL: "https://nutritfit-a2eef.firebaseio.com",
+    projectId: "nutritfit-a2eef",
+    storageBucket: "nutritfit-a2eef.appspot.com",
+    messagingSenderId: "646248193448",
+    appId: "1:646248193448:web:199e0f3fc21f1ec13f519d",
+    measurementId: "G-KYQ1YX7K38"
+  };
