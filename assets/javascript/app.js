3b32c9b245ff42bf55c09367f05d4a6ccb1bbc63
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2019-11-09 21:36:41 -0600

    addind fire base confige to app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,4 @@
 
-
   var firebaseConfig = {
     apiKey: "AIzaSyAM5HFiBF7iP96i-T_Z5q77Ijwj5FASDR0",
     authDomain: "nutritfit-a2eef.firebaseapp.com",
@@ -10,27 +9,4 @@
     appId: "1:646248193448:web:199e0f3fc21f1ec13f519d",
     measurementId: "G-KYQ1YX7K38"
   };
-   // Initialize Firebase
-   firebase.initializeApp(firebaseConfig);
-  
-
-   function signup () {
-      let email = $("#userEmail").val().trim();
-      let password = $("#password").val().trim();
-      
-     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorMessage)
-    // ...
-  });
-  
-};
-
-  function signin () {
-    
-  }
-
-$("#signup-button").on("click",signup);
 
