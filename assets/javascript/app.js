67f9e8d0ecd4178d818f69d1e48e06afb0e2178c
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2019-11-09 21:40:26 -0600

    defining sign up function inorder to create an account for client on fire base authentication

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,4 +9,20 @@
     appId: "1:646248193448:web:199e0f3fc21f1ec13f519d",
     measurementId: "G-KYQ1YX7K38"
   };
+     // Initialize Firebase
+     firebase.initializeApp(firebaseConfig);
+
+     function signup () {
+      let email = $("#userEmail").val().trim();
+      let password = $("#password").val().trim();
+      
+     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorMessage)
+    // ...
+  });
+  
+};
 
