42f689db156e365e02319359f6748d524da6c30a
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2019-11-10 18:20:22 -0600

    creating signup form and creating sign up function on js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,4 @@
-
-  var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyAM5HFiBF7iP96i-T_Z5q77Ijwj5FASDR0",
     authDomain: "nutritfit-a2eef.firebaseapp.com",
     databaseURL: "https://nutritfit-a2eef.firebaseio.com",
@@ -9,32 +8,45 @@
     appId: "1:646248193448:web:199e0f3fc21f1ec13f519d",
     measurementId: "G-KYQ1YX7K38"
   };
+
      // Initialize Firebase
      firebase.initializeApp(firebaseConfig);
+     const auth = firebase.auth();
 
-     function signup () {
-      let email = $("#userEmail").val().trim();
-      let password = $("#password").val().trim();
-      
-     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage)
-    if (errorMessage) {
+
+    //  function signup () {
+    // // 
+    //    let email = $("#signup-email").val().trim();
+    //    let password = $("#signup-password").val().trim();
         
-        $(".message-signup").html("<strong>try again! (error)</strong>"+ errorMessage)
+    //    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    //    console.log(cred.user);
+    //  });
 
-      }
-      else {
-        $(".message-signup").html("<strong>Welcome to NUTRIFIT !</strong>")
-      }
-  });
-  
-};
+    //  };
+    //  $("#signup-button").on("click",signup);
+   
 
-$("#signup-button").on("click",signup);
-
-$(document).ready(function(){
-    $('.modal-login').modal();
-  });
+    ///////////////which one is better and what is the diffrent ? 
+    //////they both creating account but also i wanna make an account and show one text that if the sucssesfuly log in or there is an error
+   
+    ////// register 
+    function signup () {
+       let email = $("#signup-email").val().trim();
+       let password = $("#signup-password").val().trim();
+     auth.createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+       const errorCode = error.code;
+       const errorMessage = error.message;
+       console.log(errorMessage)
+        if (errorMessage === '') {
+            
+            $(".message-signup").html("<strong>try again! (error)</strong>"+ errorMessage)
+    
+          }
+          else {
+            $(".message-signup").html("<strong>Welcome to NUTRIFIT !</strong>")
+          }
+      });
+  };
+  $("#signup-button").on("click",signup);
