e0a59f04cba2ce2640ceb661e78fdabdbaa1a6dc
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2019-11-10 18:25:42 -0600

    init modal at the bottom of js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -50,3 +50,58 @@ const firebaseConfig = {
       });
   };
   $("#signup-button").on("click",signup);
+
+  // $("#login-form").on("click", function(event) {
+   
+//     event.preventDefault();
+//     let email = $("#userEmail-signin").val().trim();
+//     let password = $("#password-signin").val().trim();
+//     auth.signInWithEmailAndPassword(email, password).then((cred) => {
+//         console.log(cred.user);
+//         // close the signup modal & reset form
+//         $(document).on("click", "#login-button", function(){
+//             var instance = M.Modal.getInstance(elem);
+//             instance.close();
+//           });
+
+//           //////////// which one ?
+//         // const modal = document.querySelector('#modal-login');
+//         // M.Modal.getInstance(modal).close();
+
+//        reset();
+//   });
+
+// });
+
+
+
+// login part
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = loginForm['userEmail-signin'].value;
+  const password = loginForm['password-signin'].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    // close the signup modal & reset form
+    $(document).on("click", "#login-button", function(){
+                 var instance = M.Modal.getInstance(elem);
+                  instance.close();
+                
+  });
+  loginForm.reset();
+}); 
+
+});
+
+// //init Modals
+$(document).on("click", "#login-button", function(event){
+    event.preventDefault();
+    $('.modal-login').modal();
+    console.log("Button Clicked")
+    $('#login-form').submit() 
+  });
