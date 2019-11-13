e4d3bd44f79131c321cfa65e5e7b92368b7fa741
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2019-11-13 01:57:09 -0600

    creating a function with youtube api key for adding videos from youtube to page with search

--- assets/javascript/recipe.js
+++ assets/javascript/recipe.js
@@ -1,60 +1,52 @@
 
 
+$(document).ready(function(){
 
-  $(document).ready(function(){
-
-      //careating on click event for the hole function 
-    $("#search-button").on("click", function(e) {
-      e.preventDefault();
- const queryURL = `https://www.googleapis.com/youtube/v3/search`;
+  //careating on click event for the hole function 
+$("#search-button").on("click", function(e) {
+  e.preventDefault();
+const queryURL = `https://www.googleapis.com/youtube/v3/search`;
 
 
 // Creating an AJAX call for the specific video button being clicked
 $.ajax({
-  cache: false,
-     data: $.extend({
-         key: 'AIzaSyC6r69qVvSp98MRD3JJcud6fxaN20gi6ls',
-         q: encodeURIComponent($("#search-input").val()),
-         part: 'snippet',
-         type: "video",
-     }, {
-         maxResults: 6,
-     }),
-     dataType: 'json',
-     method: 'GET',
-     timeout: 5000,
-     url: queryURL 
-    }).then(function(response) {
+cache: false,
+ data: $.extend({
+     key: 'AIzaSyC6r69qVvSp98MRD3JJcud6fxaN20gi6ls',
+     q: encodeURIComponent($("#search-input").val()),
+     part: 'snippet',
+     type: "video",
+ }, {
+     maxResults: 6,
+ }),
+ dataType: 'json',
+ method: 'GET',
+ timeout: 5000,
+ url: queryURL 
+}).then(function(response) {
 
-        $("#display-videos").empty();
-         console.log (response); 
-    
-         results = response.items;
-         for(let i=0; i<results.length; i++) {
-          //      // creating one input and give that attr
-            const $video = $("<iframe>").attr("src",`https://www.youtube.com/embed/`+results[i].id.videoId)
-           $video.addClass("video");
-            $("#display-videos").append($video);
-            console.log(results)
-         }  
-         resetVideoHeight();
-             });
-              $(window).on("resize", resetVideoHeight);
-           
-            });
+    $("#display-videos").empty();
+     console.log (response); 
 
-            function resetVideoHeight() {
-              // $(".video").css("height", "20%", "width", "30%");
-          }
-  })
+     results = response.items;
+     for(let i=0; i<results.length; i++) {
+      //      // creating one input and give that attr
+        const $video = $("<iframe>").attr("src",`https://www.youtube.com/embed/`+results[i].id.videoId)
+       $video.addClass("video");
+        $("#display-videos").append($video);
+        console.log(results)
+     }  
+
+         });
+         
+        });
+
+
+})
 
 
 
-//   var sectionTwoVideos = [
-//     '<iframe src="https://www.youtube.com/embed/j64BBgBGNIU" + width="535" + height="315" + frameborder="0" + allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" + allowfullscreen></iframe>',
-//     '<iframe src="https://www.youtube.com/embed/bl_ZSXldBrU" + width="535" + height="315" + frameborder="0" + allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" + allowfullscreen></iframe>',
-//     '<iframe src="https://www.youtube.com/embed/RE5yzqSXDCI" + width="535" + height="315" + frameborder="0" + allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" + allowfullscreen></iframe>'
-// ];
+
 
 
           
