13f07215c550209e64fb27bf5804107bc774e9a9
Adam Verma <vermaadam@gmail.com>
2019-11-13 16:17:55 -0600

    Diet Plan JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,176 +1,249 @@
-database = firebase.database; 
+  database = firebase.database;
 
-//sign up
-const signupForm = $('.input-form-signin');
-signupForm.on('click','#submit-button',function(event) {
-  event.preventDefault; 
+  // Create objects for each body type 
 
-  // get User info
-  const email = signupForm['#input-email'].val(); 
-  const password = signupForm['#input-email'].val(); 
-
-  // sign up the user
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
-
-  })
-})
-
-// create a listener for chosen foodgroupItems input to push to 
-
-// Create objects for each body type 
-
-userShape = { 
+  const userShape = {
     height: 0,
-    weight: 50,
+    weight: 0,
+    target_weight: 0,
+    waist: 0,
+    neck: 0,
+    hip: 0,
     age: 0,
     gender: '',
-    types: [
-    {   id: 'athlete',
-        rec_carbs: 3*this.weight,
-        rec_prots: 1.5*this.weight,
-        rec_fats: 0.45*this.weight,
-        diet: 'balanced',
-    },
-    {   id: 'active',
-        rec_carbs: 2*this.weight,
-        rec_prots: 1.1*this.weight,
-        rec_fats: 0.4*this.weight,
+    types: [{
+        id: 'athletic',
+        rec_carbs: 3 * this.weight,
+        rec_prots: 1.5 * this.weight,
+        rec_fats: 0.45 * this.weight,
+        diet: 'high-carb',
+        activity_factor: 1.9,
+      },
+      {
+        id: 'active',
+        rec_carbs: 2 * this.weight,
+        rec_prots: 1.1 * this.weight,
+        rec_fats: 0.4 * this.weight,
         diet: 'high-protein',
-    },
-    {   id: 'sedentary',
-        rec_carbs: 0.9*this.weight,
-        rec_prots: 0.7*this.weight,
-        rec_fats: 0.4*this.weight,
+        activity_factor: 1.725,
+      },
+      {
+        id: 'sedentary',
+        rec_carbs: 0.9 * this.weight,
+        rec_prots: 0.7 * this.weight,
+        rec_fats: 0.4 * this.weight,
+        diet: 'balanced',
+        activity_factor: 1.35,
+      },
+      {
+        id: 'overweight',
+        rec_carbs: 0.7 * this.weight,
+        rec_prots: 0.7 * this.weight,
+        rec_fats: 0.3 * this.weight,
+        diet: 'low-fat',
+        activity_factor: 1.2,
+      },
+      {
+        id: 'obese',
+        rec_carbs: 0.5 * this.weight,
+        rec_prots: 0.5 * this.weight,
+        rec_fats: 0.25 * this.weight,
         diet: 'low-carb',
-    },
-    {   id: 'overweight',
-        rec_carbs: 0.7*this.weight,
-        rec_prots: 0.7*this.weight,
-        rec_fats: 0.3*this.weight,
-        diet: 'low-fat',
-    },
-    {   id: 'obese',
-        rec_carbs: 0.5*this.weight,
-        rec_prots: 0.5*this.weight,
-        rec_fats: 0.25*this.weight,
-        diet: 'low-fat',
-    }]
-};
+        activity_factor: 1.1,
+      }
+    ]
+  };
+
+  // // This function handles events where a foodGroupItem box is checked
+  $("#submit-question").on("click", function (event) {
+    event.preventDefault();
+    userFoodPref = []
+    $('input:checkbox:checked').each(function () {
+      userFoodPref.push($(this).val());
+    });
+    $('#GFG_DOWN').text(userFoodPref);
+    console.log(userFoodPref)
+
+    // Create a function that performs an API call for the BMR 
+
+      userShape.height = parseInt($('#height-input').val());
+      userShape.weight = parseInt($('#weight-input').val());
+      userShape.target_weight = parseInt($('#target-weight-input').val());
+      goal_weight_diff = userShape.weight - userShape.target_weight;
+      dob = $('#dob-input').val();
+      userShape.age = Math.abs(moment().diff(dob, 'years'));
+      userShape.gender = toString($('#gender-input').val());
+
+      for (let i = 0; i < userShape.types.length; i++) {
+        const typeDiet = userShape.types[i].diet;
+        const typeId = userShape.types[i].id;
+        const typeActivity = userShape.types[i].activity_factor;
+        let userExercise = $('.form-check-exercise').val()
+
+          if (userExercise === "Do not exercise") {
+            
+          }
+          else if (userExercise === "1-2 days") {
+            
+          }
+          else if (userExercise === "3-5 days") {
+           
+          }
+          else if (userExercise === "6-7 days") {
+            
+          }
+          else if (userExercise === "7 days") {
+            
+          }
+        }
+      
+
+      let bmr, daily_intake_cals
+      if (userShape.gender === "Male") {
+        bmr = 655 + (4.35 * userShape.weight) + (4.7 * userShape.height) - (4.7 * userShape.age);
+        daily_intake_cals = parseFloat(bmr * typeActivity);
+      } else {
+        bmr = 66 + (6.23 * userShape.weight) + (12.7 * userShape.height) - (6.8 * userShape.age);
+        daily_intake_cals = parseFloat(bmr * typeActivity);
+      } 
+      
+    
+      const bmi = parseFloat((userShape.weight / (userShape.height * userShape.height)) * 703).toFixed(1);
+
+      if (bmi < 18.5) {
+        typeId = 'underweight'
+      } else if (bmi >= 18.5) {
+        typeId = 'active'
+      } else if (bmi >= 25) {
+        typeId = 'sedentary'
+      } else if (bmi >= 30) {
+        typeId = 'overweight'
+      } else if (bmi >= 35) {
+        typeId = 'obese'
+      }
+
+      const female_body_fat = (163.205 * Math.log10(userShape.hip + userShape.hip - userShape.neck)) - (97.684 * Math.log10(userShape.height)) - 78.387
+      const male_body_fat = (86.010 * Math.log10(userShape.waist - userShape.neck)) - (70.041 * Math.log10(userShape.height)) + 36.76
+
+      if (userShape.gender === 'Female') {
+        let fatEl = $('<p>').text(`${female_body_fat}%`);
+        bodyfatDiv = $("<div class= 'body-fat'>").append(fatEl);
+        $('.health-info').append('.body-fat');
+      } else if (userShape.gender === 'Male') {
+        let fatEl = $('<p>').text(`${male_body_fat}%`);
+        bodyfatDiv = $("<div class= 'body-fat'>").append(fatEl);
+        $('.health-info').append('.body-fat');
+      }
+      displayNutritionInfo(typeDiet, daily_intake_cals)
+    });
 
   // displayNutritionInfo function re-renders the HTML to display the appropriate content
-  function displayNutritionInfo() {
-
-    const foodGroupItem = ['chicken'];
-    for (let i=0; i<userShape.types.length; i++) {
-        typeDiet = userShape.types[i].diet;
-        typeId = userShape.types[i].id; 
-        console.log(typeDiet);
-    
-    const  queryURL =
+  function displayNutritionInfo(typeDiet, foodGroupItem, daily_intake_cals) {
+    console.log(typeDiet, daily_intake_cals);  
+    const queryURL =
       `https://api.edamam.com/search?q=${foodGroupItem}&diet=${typeDiet}&app_id=72b0f0df`;
-    console.log(queryURL);
+
     // Creating an AJAX call for the specific gif button being clicked
     $.ajax({
       url: queryURL,
-      method: "GET"
+      method: "GET",
     }).then(function (response) {
       const results = response.hits;
-
       // Creating a div to hold the diet information
-     
       for (let i = 0; i < results.length; i++) {
-      const dietDiv = $("<div class='diet'>");
-      //Storing the recipe name, calories, image 
-      const label = results[i].recipe.label; 
-      const calories = results[i].recipe.calories;
-      const imageURL = results[i].recipe.image;
+        const single_serving = parseFloat(1 / parseInt(results[i].recipe.yield)).toFixed(2);
 
-      // Creating an element to have the label, calories, image displayed
-      const labEl = $("<p>").text(label);
-      const caloriesEl = $("<p>").text(parseInt(calories)); 
-      const img = $("<img>").addClass("recipeImg");
+        const dietChoiceButton = $("<button class='diet'>");
+        //Storing the recipe name, calories, image 
+        const label = results[i].recipe.label;
+        const fat = parseInt(results[i].recipe.totalNutrients.FAT * single_serving) + "g";
+        const carb = parseInt(results[i].recipe.totalNutrients.CHOCDF * single_serving) + "g";
+        const protein = parseInt(results[i].recipe.totalNutrients.FAT * single_serving) + "g";
+        const calories = parseInt(results[i].recipe.calories * single_serving);
+        
+        const recipeImgURL = results[i].recipe.image;
+        
+        // Creating an element to have the label, calories, image displayed
 
-       // Retrieving the image for the 
-      dietDiv.append(labEl,caloriesEl,img);
+        const labEl = $("<p>").text(label);
+        const caloriesEl = $("<p>").text(calories);
+        const fatEl = $("<p>").text(fat);
 
-      // Putting the entire gif above the previous gifs
-      $("#recipe-view").prepend(dietDiv);
+        const img = $("<img>").attr("src", recipeImgURL).addClass("recipeImage");
+
+        // Retrieving the image for the 
+        dietChoiceButton.append(labEl, caloriesEl, fatEl, img);
+
+        // Putting the entire gif above the previous gifs
+        $("#breakfast").append(dietChoiceButton);
+
+        $(".diet").on("click", function (e) {
+
+        });
       }
     });
   };
-}
 
-  // This function handles events where a foodGroupItem box is checked
-  $("#add-foodGroupItem").on("click", function (event) {
-    event.preventDefault();
-    // This line grabs the input from the textbox
-    const checkedItem = $("#checkbox-foodItem").val().trim();
+  // Adding food item from the textbox to our array
+  // foodGroupItem.push(checkedItem);
 
-    // Adding food item from the textbox to our array
-    foodGroupItem.push(checkedItem);
-
-    // Calling renderButtons which handles the processing of our foodGroupItem array
-    renderButtons();
-  });
-
-  // Adding a click event listener to all elements with a class of "gif-btn"
-  $(document).on("click", ".diet", displayNutritionInfo);
+  // Calling renderButtons which handles the processing of our foodGroupItem array
 
 
 
-bodyIndex = {
 
-   // Create a function that performs an API call for the BMR 
-   displaybmrInfo = function() {
-    
-    const userHeight = parseInt($(this).attr("data-height"));
-    const userWeight = parseInt($(this).attr("data-weight"));
-    const userAge =  pareInt($(this).attr("data-age"));
-    const userGender =  toString($(this).attr("data-gender"));
-    
-    bmrFormula = {
-    const gender: '',
-    female_bmr: 655 + (4.35 * weight) + (4.7 * height) - (4.7 * age),
 
-    male_bmr: 66 + (6.23 * weight) + (12.7 * height) - (6.8 * age),
-   };
 
-   bmiFormula = (weight / (height * height)) * 703;
+  // // // YouTube API 
 
-  bodyFatCalculator = function() {
-    const bodyCF = []; 
-    female_body_fat =[163.205*Math.log10(waist + hip - neck)] - [97.684 * Math.log10(height)] - 78.387
-    male_body_fat = [86.010*Math.log10(waist - neck)] - [70.041 * Math.log10(height)] + 36.76
-  }
-}
-}
+  // function displayVideoInfo() {
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyAM5HFiBF7iP96i-T_Z5q77Ijwj5FASDR0",
-    authDomain: "nutritfit-a2eef.firebaseapp.com",
-    databaseURL: "https://nutritfit-a2eef.firebaseio.com",
-    projectId: "nutritfit-a2eef",
-    storageBucket: "nutritfit-a2eef.appspot.com",
-    messagingSenderId: "646248193448",
-    appId: "1:646248193448:web:199e0f3fc21f1ec13f519d",
-    measurementId: "G-KYQ1YX7K38"
-  };
-     // Initialize Firebase
-     firebase.initializeApp(firebaseConfig);
+  //   const  queryURL =
+  //     `https://www.googleapis.com/youtube/v3/search`;
 
-     function signup () {
-      let email = $("#userEmail").val().trim();
-      let password = $("#password").val().trim();
-      
-     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorMessage)
-    // ...
-  });
-  
-};
+  //   // Creating an AJAX call for the specific gif button being clicked
+  //   $.ajax({
+  //       cache: false,
+  //          data: $.extend({
+  //              key: 'AIzaSyC6r69qVvSp98MRD3JJcud6fxaN20gi6ls',
+  //              q: $('').val(),
+  //              part: 'snippet'
+  //          }, {
+  //              maxResults: 20,
+  //              pageToken: $("#pageToken").val()
+  //          }),
+  //          dataType: 'json',
+  //          method: 'GET',
+  //          timeout: 5000,
+  //          url: queryURL,
+  //      }).done(function(data) {
+  //          $('.btn-group').show();
+  //          if (typeof data.prevPageToken === "undefined") {
+  //              $("#pageTokenPrev").hide();
+  //          } else {
+  //              $("#pageTokenPrev").show();
+  //          }
+  //          if (typeof data.nextPageToken === "undefined") {
+  //              $("#pageTokenNext").hide();
+  //          } else {
+  //              $("#pageTokenNext").show();
+  //          }
+  //          var items = data.items,
+  //              videoList = "";
+  //          $("#pageTokenNext").val(data.nextPageToken);
+  //          $("#pageTokenPrev").val(data.prevPageToken);
+  //          $.each(items, function(index, e) {
+  //              videoList = videoList + '<li class="hyv-video-list-item"><div class="hyv-content-wrapper"><a href="" class="hyv-content-link" title="' + e.snippet.title + '"><span class="title">' + e.snippet.title + '</span><span class="stat attribution">by <span>' + e.snippet.channelTitle + '</span></span></a></div><div class="hyv-thumb-wrapper"><a href="" class="hyv-thumb-link"><span class="hyv-simple-thumb-wrap"><img alt="' + e.snippet.title + '" src="' + e.snippet.thumbnails.default.url + '" width="120" height="90"></span></a></div></li>';
+  //          });
+  //          $("#hyv-watch-related").html(videoList);
+  //          // JSON Responce to display for user 	 
+  //          new PrettyJSON.view.Node({
+  //              el: $(".hyv-watch-sidebar-body"),
+  //              data: data
+  //          });
+  //      });
+  //      console.log(queryURL);
+  //     // Creating a div to hold the diet information
+  //     }
 
-$("#signup-button").on("click",signup);
+  //
