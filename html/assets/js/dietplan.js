b29a6e842b198839c1203caa2dde8ada63170859
Chae Jung <chaej_ung@outlook.com>
2019-11-14 20:06:01 -0600

    sending all files I worked on to merge

--- html/assets/js/dietplan.js
+++
@@ -1,4 +0,0 @@
-// setting button to log out and go to the login page on click 
-$("#log-out-button").on("click", function() {
-    location.href="loginpage.html";
-});
--- html/assets/js/mealprep.js
+++
@@ -1,9 +0,0 @@
-// setting the bootstrap carousel
-$('#blogCarousel').carousel({
-    interval: 3000
-});
-
-// setting button to log out and go to the login page on click 
-$("#log-out-button").on("click", function() {
-    location.href="loginpage.html";
-});
